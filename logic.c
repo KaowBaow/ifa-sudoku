0357afdb28692451a735d24ec128ca8d5e130d9e
Philipp Tiemann <p.tiemann@anedo.de>
2022-06-25 12:09:18 +0200

    change to new version

--- logic.c
+++ logic.c
@@ -1,31 +1,31 @@
 #include "sudoku.h"
 
 
-void report(char *s,int i,int j)
+void sudokuError(char *s,int i,int j)
 {
 printf("\nThe sudoku is INCORRECT");
 printf("\nin %s. Row:%d,Column:%d",s,i+1,j+1);
-//getch();
 exit(0);
 }
 
 void checkSudoku(int a[i][j]){
-/*++++++++++++++++++
-checking rows
-+++++++++++++++++++
-we check each cell in each row.
-We start with a flag 0x0000.
-if 1 is found zeroth bit of flag is set.
-if 2 is found, first bit is set
-and so on.
-If all digits 1 to 9 are present, flag's value
-will be 0x01FF.
-If flag is 0x01FF after traversing a row,
-the row has all numbers 1 to 9.
-So, it is correct.
-If the flag is not 0x01FF after traversing a row,
-the row is incorrectly filled.
-Then we call report() function
+
+    sudokuError()
+/*
+Prüfung der Zeilen
+wir prüfen jede Zelle in jeder Zeile.
+Wir beginnen mit einem Flag 0x0000.
+wenn 1 gefunden wird, wird das nullte Bit des Flags gesetzt.
+wenn 2 gefunden wird, wird das erste Bit gesetzt und so weiter.
+Wenn alle Ziffern 1 bis 9 vorhanden sind, ist der Wert des Flags
+0x01FF.
+Wenn das Flag 0x01FF ist, nachdem eine Zeile durchlaufen wurde,
+hat die Zeile alle Ziffern 1 bis 9.
+Wenn das Flag nach dem Durchlaufen einer Zeile nicht 0x01FF ist,
+ist die Zeile nicht korrekt ausgefüllt.
+Dann rufen wir die Funktion sudokuError() auf.
+Die selbe Logik kommt bei Spalten und den kleinen Quadraten zum einsatzt.
+
 */
 for(i=0;i<9;i++)
     {
@@ -33,14 +33,12 @@ for(i=0;i<9;i++)
     for(j=0;j<9;j++)
         flag|=1<<(a[i][j]-1);
     if(flag!=0x01FF)
-        report("row",i,j-1);
+        sudokuError("row",i,j-1);
     }
 
-/*++++++++++++++++++
-checking columns
-+++++++++++++++++++
-Just like row checking.
-The flag is for a column.
+/*
+Prüfung von Spalten
+Das Flag steht für eine Spalte.
 */
 for(j=0;j<9;j++)
     {
@@ -48,13 +46,11 @@ for(j=0;j<9;j++)
     for(i=0;i<9;i++)
         flag|=1<<(a[i][j]-1);
     if(flag!=0x01FF)
-        report("col",i-1,j);
+        sudokuError("col",i-1,j);
     }
-/*++++++++++++++++++
-checking Squares (3x3)
-+++++++++++++++++++
-Just like row checking.
-The flag is for a square.
+/*
+Überprüfung von Quadraten (3x3)
+Das flag steht für ein Quadrat.
 */
 for(si=0;si<3;si++)
     {
@@ -68,7 +64,7 @@ for(si=0;si<3;si++)
 
             }
         if(flag!=0x01FF)
-                report("square",si*3+i-1,sj*3+j-1);
+                sudokuError("square",si*3+i-1,sj*3+j-1);
         }
     }
 printf("\nThe sudoku is correct");
