a49f95bc6697041ef9587c294fa08f69bc7dddf7
Philipp Tiemann <p.tiemann@anedo.de>
2022-06-25 12:49:22 +0200

    fixed logic.c

--- logic.c
+++ logic.c
@@ -3,69 +3,80 @@
 
 void sudokuError(char *s,int i,int j)
 {
-printf("\nThe sudoku is INCORRECT");
-printf("\nin %s. Row:%d,Column:%d",s,i+1,j+1);
-exit(0);
+    printf("\n The sudoku is INCORRECT");
+    printf("\n in %s. Row:%d,Column:%d",s,i+1,j+1);
+    exit(0);
 }
 
-void checkSudoku(int a[i][j]){
+void checkSudoku(int a[i][j])
+{
 
-    sudokuError()
-/*
-Prüfung der Zeilen
-wir prüfen jede Zelle in jeder Zeile.
-Wir beginnen mit einem Flag 0x0000.
-wenn 1 gefunden wird, wird das nullte Bit des Flags gesetzt.
-wenn 2 gefunden wird, wird das erste Bit gesetzt und so weiter.
-Wenn alle Ziffern 1 bis 9 vorhanden sind, ist der Wert des Flags
-0x01FF.
-Wenn das Flag 0x01FF ist, nachdem eine Zeile durchlaufen wurde,
-hat die Zeile alle Ziffern 1 bis 9.
-Wenn das Flag nach dem Durchlaufen einer Zeile nicht 0x01FF ist,
-ist die Zeile nicht korrekt ausgefüllt.
-Dann rufen wir die Funktion sudokuError() auf.
-Die selbe Logik kommt bei Spalten und den kleinen Quadraten zum einsatzt.
 
-*/
-for(i=0;i<9;i++)
-    {
-    flag=0x0000;
-    for(j=0;j<9;j++)
-        flag|=1<<(a[i][j]-1);
-    if(flag!=0x01FF)
-        sudokuError("row",i,j-1);
-    }
+    /*
+    Prüfung der Zeilen
+    wir prüfen jede Zelle in jeder Zeile.
+    Wir beginnen mit einem Flag 0x0000.
+    wenn 1 gefunden wird, wird das nullte Bit des Flags gesetzt.
+    wenn 2 gefunden wird, wird das erste Bit gesetzt und so weiter.
+    Wenn alle Ziffern 1 bis 9 vorhanden sind, ist der Wert des Flags
+    0x01FF.
+    Wenn das Flag 0x01FF ist, nachdem eine Zeile durchlaufen wurde,
+    hat die Zeile alle Ziffern 1 bis 9.
+    Wenn das Flag nach dem Durchlaufen einer Zeile nicht 0x01FF ist,
+    ist die Zeile nicht korrekt ausgefüllt.
+    Dann rufen wir die Funktion sudokuError() auf.
+    Die selbe Logik kommt bei Spalten und den kleinen Quadraten zum einsatzt.
 
-/*
-Prüfung von Spalten
-Das Flag steht für eine Spalte.
-*/
-for(j=0;j<9;j++)
+    */
+    for(i=0; i<9; i++)
     {
-    flag=0x0000;
-    for(i=0;i<9;i++)
-        flag|=1<<(a[i][j]-1);
-    if(flag!=0x01FF)
-        sudokuError("col",i-1,j);
-    }
-/*
-Überprüfung von Quadraten (3x3)
-Das flag steht für ein Quadrat.
-*/
-for(si=0;si<3;si++)
-    {
-    for(sj=0;sj<3;sj++)
-        {
         flag=0x0000;
-        for(i=0;i<3;i++)
-            {
-            for(j=0;j<3;j++)
-                flag|=1<<(a[si*3+i][sj*3+j]-1);
-
-            }
+        for(j=0; j<9; j++)
+        {
+            flag|=1<<(a[i][j]-1);
+        }
         if(flag!=0x01FF)
-                sudokuError("square",si*3+i-1,sj*3+j-1);
+        {
+            sudokuError("row",i,j-1);
         }
     }
-printf("\nThe sudoku is correct");
+
+    /*
+    Prüfung von Spalten
+    Das Flag steht für eine Spalte.
+    */
+    for(j=0; j<9; j++)
+    {
+        flag=0x0000;
+        for(i=0; i<9; i++)
+        {
+            flag|=1<<(a[i][j]-1);
+        }
+        if(flag!=0x01FF)
+        {
+            sudokuError("col",i-1,j);
+        }
+    }
+    /*
+    Überprüfung von Quadraten (3x3)
+    Das flag steht für ein Quadrat.
+    */
+    for(si=0; si<3; si++)
+    {
+        for(sj=0; sj<3; sj++)
+        {
+            flag=0x0000;
+            for(i=0; i<3; i++)
+            {
+                for(j=0; j<3; j++)
+                    flag|=1<<(a[si*3+i][sj*3+j]-1);
+
+            }
+            if(flag!=0x01FF)
+            {
+                sudokuError("square",si*3+i-1,sj*3+j-1);
+            }
+        }
+    }
+    printf("\nThe sudoku is correct");
 }
